1ad65cbff74f6a5af53e0d5dfbcc61bb246a2d92
Egesa Raymond <111632110+EgesaR@users.noreply.github.com>
2024-06-30 15:56:44 +0000

    Other Commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar.js";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["vietnamese"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className} overflow-x-hidden`}
       >
+        <SpeedInsights />
         <Navbar />
         <div className="mt-[60px] h-[calc(100vh-62.5px)] w-100 overflow-hidden">{children}</div>
       </body>
