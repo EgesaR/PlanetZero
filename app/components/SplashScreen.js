c8c0098f1c2a17f3a1ca27341ec04d0086a1d5bf
Egesa Raymond <111632110+EgesaR@users.noreply.github.com>
2024-07-07 09:33:42 +0000

    Just another one

--- app/components/SplashScreen.js
+++ app/components/SplashScreen.js
@@ -14,7 +14,7 @@ const SplashScreen = ({ finishLoading }) => {
       targets: "#playlist",
       delay: 0,
       scale: 1,
-      duration: 15500,
+      duration: 16500,
       easing: "easeInOutExpo",
     });
   };
