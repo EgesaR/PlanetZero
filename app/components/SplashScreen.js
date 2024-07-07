4d1fd1eaabd655bbb10d8b0e00ec74e6dfe47ed8
Egesa Raymond <111632110+EgesaR@users.noreply.github.com>
2024-07-07 09:03:36 +0000

    Just another commit

--- app/components/SplashScreen.js
+++ app/components/SplashScreen.js
@@ -20,7 +20,7 @@ const SplashScreen = ({ finishLoading }) => {
   };
 
   useEffect(() => {
-    const timeOut = setTimeout(() => setIsMounted(true), 700);
+    const timeOut = setTimeout(() => setIsMounted(true), 1700);
     animate();
     return () => clearTimeout(timeOut);
   }, []);
