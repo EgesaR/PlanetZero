88b53a131e26c92fca1d6c4ab28c8e764895b6c7
Egesa Raymond <111632110+EgesaR@users.noreply.github.com>
2024-07-28 16:31:24 +0000

    July 28 19:31 commit

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import Navbar from "./components/Navbar.js";
 import SplashScreen from "./components/SplashScreen.js";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["vietnamese"] });
 
@@ -27,19 +28,44 @@ export default function RootLayout({ children }) {
   useEffect(() => {
     if (isLoading) return;
   }, [isLoading]);
+  const user = null;
   return (
     <html lang="en">
       <body className={`${inter.className} overflow-x-hidden`}>
-        {isLoading && isHome ? (
-          <SplashScreen finishLoading={() => setIsLoading(false)} />
-        ) : (
-          <>
-            <SpeedInsights />
-            <div className="{mt-[60px]} {h-[calc(100vh-62.5px)]} h-[100vh] w-100 overflow-hidden">
-              {children}
-            </div>
-          </>
-        )}
+        {user ? (<>
+          {isLoading && isHome ? (
+            <SplashScreen finishLoading={() => setIsLoading(false)} />
+          ) : (
+            <>
+              <SpeedInsights />
+              <div className="{mt-[60px]} {h-[calc(100vh-62.5px)]} h-[100vh] w-100 overflow-hidden">
+                {children}
+              </div>
+            </>
+          )}
+        </>) : (
+      <div>
+        <Navbar />
+        <div className="h-[calc(100vh-62.5px)] mt-[62px] w-full">
+          <div className="flex flex-col sm:flex-row h-[100vh] justify-center items-center gap-16 ">
+              <div className="px-8">
+                <h2 className="text-4xl font-semibold">Participate and save our planet </h2>
+                <h4 className="text-2xl text-slate-800">Create blogs that will optimistically change our planet for the better...</h4>
+              </div>
+              <div>
+                <Image
+                  src="/siginimage.jpg"
+                  alt="Descriptive text for screen readers"
+                  width={200}
+                  height={200}
+                  className="flex-1 h-full"
+                  layout="responsive"
+                />
+              </div>
+          </div>
+        </div>
+      </div>
+    )}
       </body>
     </html>
   );
